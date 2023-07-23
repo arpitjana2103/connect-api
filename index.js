a69066b735c8e242cea5fafd93cefa20c100f9e9
Arpit Jana <arpitjana2103@gmail.com>
2023-07-23 23:20:06 +0530

    Core Issue Resolved

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const {orderRouter} = require('./Routes/order.routes.js');
 const app = express();
 const PORT = process.env.PORT || 4500;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
