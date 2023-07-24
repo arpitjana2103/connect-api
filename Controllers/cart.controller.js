b2e7935e1bccd9900a6f6e2451a765599b040735
Arpit Jana <arpitjana2103@gmail.com>
2023-07-24 12:28:45 +0530

    Version key

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -1,51 +1,51 @@
 const {CartModel} = require('../Models/cart.model.js');
 const {UserModel} = require('../Models/user.model.js');
 
-function validateCartData(cartData) {
-    const errors = {};
+// function validateCartData(cartData) {
+//     const errors = {};
 
-    // Check if required fields are missing or incorrect
-    if (!cartData.userEmail) {
-        errors.userEmail = 'User Email required';
-    }
-    if (
-        !cartData.products ||
-        !Array.isArray(cartData.products) ||
-        cartData.products.length === 0
-    ) {
-        errors.products = 'At least one product with quantity is required.';
-    } else {
-        for (const product of cartData.products) {
-            if (!product.productID) {
-                errors.products = 'Product ID is required for each product.';
-                break;
-            }
-            if (
-                !product.quantity ||
-                typeof product.quantity !== 'number' ||
-                product.quantity <= 0
-            ) {
-                errors.products =
-                    'Quantity should be a positive number for each product.';
-                break;
-            }
-        }
-    }
-    if (cartData.empty === undefined || typeof cartData.empty !== 'boolean') {
-        errors.empty = 'Empty field is required and should be a boolean.';
-    }
-    if (
-        !cartData.totalPrice ||
-        typeof cartData.totalPrice !== 'number' ||
-        cartData.totalPrice <= 0
-    ) {
-        errors.totalPrice =
-            'Total price is required and should be a positive number.';
-    }
+//     // Check if required fields are missing or incorrect
+//     if (!cartData.userEmail) {
+//         errors.userEmail = 'User Email required';
+//     }
+//     if (
+//         !cartData.products ||
+//         !Array.isArray(cartData.products) ||
+//         cartData.products.length === 0
+//     ) {
+//         errors.products = 'At least one product with quantity is required.';
+//     } else {
+//         for (const product of cartData.products) {
+//             if (!product.productID) {
+//                 errors.products = 'Product ID is required for each product.';
+//                 break;
+//             }
+//             if (
+//                 !product.quantity ||
+//                 typeof product.quantity !== 'number' ||
+//                 product.quantity <= 0
+//             ) {
+//                 errors.products =
+//                     'Quantity should be a positive number for each product.';
+//                 break;
+//             }
+//         }
+//     }
+//     if (cartData.empty === undefined || typeof cartData.empty !== 'boolean') {
+//         errors.empty = 'Empty field is required and should be a boolean.';
+//     }
+//     if (
+//         !cartData.totalPrice ||
+//         typeof cartData.totalPrice !== 'number' ||
+//         cartData.totalPrice <= 0
+//     ) {
+//         errors.totalPrice =
+//             'Total price is required and should be a positive number.';
+//     }
 
-    // Return errors object. If it's empty, then there are no errors.
-    return Object.keys(errors).length > 0 ? errors : null;
-}
+//     // Return errors object. If it's empty, then there are no errors.
+//     return Object.keys(errors).length > 0 ? errors : null;
+// }
 
 const addCart = async function (req, res) {
     try {
--- Models/cart.model.js
+++ Models/cart.model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-const CartSchema = new mongoose.Schema({
-    userEmail: {type: String, required: true, index: true},
-    products: {type: String, required: true, index: true},
-});
+const CartSchema = new mongoose.Schema(
+    {
+        userEmail: {type: String, required: true, index: true},
+        products: {type: String, required: true, index: true},
+    },
+    {versionKey: false}
+);
 
 const CartModel = mongoose.model('cart', CartSchema);
 module.exports = {CartModel};
